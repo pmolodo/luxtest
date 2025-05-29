12afbdba09cfdc50a29e55a2ed3ab9e532afeb29
Paul Molodowitch <pmolodowitch@nvidia.com>
2025-05-29 13:04:06 -0700

    add set_py_env.bash

--- luxtest_python.sh
+++ luxtest_python.sh
@@ -5,16 +5,6 @@ set -u
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export LUXTEST_VENV_NAME=.venv
-
-. "${THIS_DIR}/uv_install.sh"
-
-if ! [ -f "${LUXTEST_VENV_ACTIVATE}" ]; then
-    "${LUXTEST_UV_PATH}" --quiet venv --managed-python "${LUXTEST_VENV_NAME}"
-fi
-
-. "${LUXTEST_VENV_ACTIVATE}" &> /dev/null
-
-"${LUXTEST_UV_PATH}" --quiet sync
+. "${THIS_DIR}/set_py_env.bash"
 
 python "$@"
